8e2cd29d2fe630e4038151899d4d664cdf08040d
Kyrla Nunes <kyrlang@bol.com.br>
2018-10-15 20:56:03 -0300

    add ws

---
+++ WS/TransmissorMessagem.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="TransmissorMessagem.asmx.cs" Class="WS.TransmissorMessagem" %>
---
+++ WS/TransmissorMessagem.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using WS.WCF;
+
+namespace WS
+{
+    /// <summary>
+    /// Summary description for TransmissorMessagem
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class TransmissorMessagem : System.Web.Services.WebService
+    {
+
+        [WebMethod()]
+        public bool TransmitirMensagem(Pacientes pacientes)
+        {
+            TransmissorMensagemClient client = new TransmissorMensagemClient();
+            try
+            {
+                client.InserirMensagem(pacientes);
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
